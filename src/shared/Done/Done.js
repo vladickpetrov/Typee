93d78b74806d5cb432fff018404ccb2a8e011167
vladickpetrov <minnesota777@mail.ru>
2023-06-07 17:35:26 +0300

    Update Done.js

--- src/shared/Done/Done.js
+++ src/shared/Done/Done.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Done.css";
 
 function Done() {
-  return <div className="done">Молодец!</div>;
+  return <div className="done">Good job!</div>;
 }
 
 export default Done;
